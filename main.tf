6a2bf494e4d7424ad2cec4885eb98ed0ec86257d
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2023-05-26 12:32:54 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,43 @@
+// main.tf
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "ami" {
+  description = "Amazon Machine Image ID"
+  type        = string
+  default     = "ami-0c94855ba95c71c99" // Ubuntu 20.04 LTS
+}
+
+resource "aws_instance" "example_instance" {
+  ami           = var.ami
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "ExampleInstance"
+  }
+}// main.tf
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "ami" {
+  description = "Amazon Machine Image ID"
+  type        = string
+  default     = "ami-0c94855ba95c71c99" // Ubuntu 20.04 LTS
+}
+
+resource "aws_instance" "example_instance" {
+  ami           = var.ami
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "ExampleInstance"
+  }
+}
